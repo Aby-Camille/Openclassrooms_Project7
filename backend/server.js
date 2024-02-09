c1e6cc575fd2c485bcabb5f5df1a0352318aa900
Camille L <camille0756@gmail.com>
2024-02-09 05:10:28 +0100

    start mongoDB mongoose and .env install

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ const app = require('./app');
 const normalizePort = val => {
     const port = parseInt(val, 10);
 
+    if (isNaN(port)) {
+        return val;
+    }
     if (port >= 0) {
         return port;
       }
