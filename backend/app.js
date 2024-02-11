e11762fce67c9e7c5b8033f536f8c2a63451161f
Camille L <camille0756@gmail.com>
2024-02-11 16:12:12 +0100

    models book / user

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require ('express');
 const mongoose = require('mongoose');
+const Book = require('./models/book');
+const User = require('./models/user');
 const app = express();
 require('dotenv').config();
 
@@ -13,12 +15,81 @@ mongoose.connect(dataBaseUrl,
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use(express.json());
-app.use((req, res, next) => {
+app
+.use(express.json())
+.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+})
+// Books
+.get('/api/books', async(req, res) => {
+  const books = await Book.find({});
+  res.json(books);
+})
+.post('/api/books', async(req, res) => {
+  const book = new Book(req.body);
+
+  try {
+    await book.save();
+    res.json({ message: 'Livre sauvegardé' });
+  } catch(error) {
+    console.error(error);
+    res.statusCode(500);
+    res.statusMessage(error);
+  }
+})
+// Book
+.get('/api/books/:id', async(req, res) => {
+  const book = await Book.findById(req.params.id);
+
+  if (book) {
+    res.json(book);
+    return;
+  }
+
+  res.sendStatus(404);
+})
+.put('/api/books/:id', async(req, res) => {
+  
+})
+.delete('/api/books/:id', async(req, res) => {
+  try {
+    await Book.deleteOne({ _id: req.params.id });
+    res.json({ message: 'Livre supprimé' });
+  } catch(error) {
+    console.error(error);
+    res.statusMessage(error);
+    res.sendStatus(500);
+  }
+})
+// Authent
+.post('/api/auth/login', async(req, res) => {
+  // hachage du mot de passe
+  const password = req.body.password;
+  const user = await User.findOne({ email: req.body.email, password });
+
+  if (user) {
+    res.json({ usrId: user._id, token: '' });
+    return;
+  }
+
+  res.sendStatus(404);
+})
+.post('/api/auth/signup', async(req, res) => {
+  // hacher le mot de passe
+  const password = req.body.password;
+  const user = new User({ email: req.body.email, password });
+
+  try {
+    await user.save();
+    res.sendStatus(200);
+  } catch(error) {
+    console.error(error);
+    res.statusMessage(error);
+    res.sendStatus(500);
+  }
+})
 
 module.exports = app;
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,3 +1,8 @@
 const mongoose = require ('mongoose');
 
-module.exports = mongoose.model();
+const User = mongoose.model('User', {
+  email: String,
+  password: String
+});
+
+module.exports = User;
