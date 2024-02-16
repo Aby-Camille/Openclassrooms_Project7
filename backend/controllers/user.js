37905d602b745b4d2a627fa2a7c5f8f95b2cfc5d
Camille L <camille0756@gmail.com>
2024-02-16 03:15:33 +0100

    creating login token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,33 +1,48 @@
 const User = require('./models/user');
+const bcrypt = require('bcrypt');
+const jwt = ('jsonwebtoken');
+require('dotenv').config;
 
 //Signup
-exports.userSignup = async(req, res) => {
-    // hacher le mot de passe
+exports.userSignup = (req, res) => {
     const password = req.body.password;
-    const user = new User({ email: req.body.email, password });
-  
-    try {
-      await user.save();
-      res.sendStatus(200);
-    } catch(error) {
-      console.error(error);
-      res.statusMessage(error);
-      res.sendStatus(500);
-    }
-  };
+    bcrypt.hash(password, 10)
+    .then (hash => {
+    const user = new User({ 
+        email: req.body.email, 
+        password: hash 
+    });
+    user.save()
+      .then (() => res.status(201).json({ message: 'Nouvel utilisateur enregistré'}))
+      .catch(error => res.status(400).json({ error }))
+    })
+    .catch(error => res.status(500).json({ error }));
+};
 
 //Login
-exports.userLogin = async(req, res) => {
+exports.userLogin = async(req, res, next) => {
     
     const password = req.body.password;
-    const user = await User.findOne({ email: req.body.email, password });
-  
-    if (user) {
-      res.status(200).json({
-         usrId: user._id, 
-         token: 'jwt' });
-      return;
-    }
-  
-    res.sendStatus(404);
-  };
+    User.findOne({ email: req.body.email, password })
+    .then (user => {
+        if (!user) {
+            return res.status(401).json({ error: 'Utilisateur ou mot de passe incorrete' });
+        }
+            bcrypt.compare(req.body.password, user.password)
+            .then(valid => {
+                if (!valid) {
+                    return res.status(401).json({ error: 'Mot de passe incorrecte' });
+                }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: jwt.sign(
+                            { userId: user._id },
+                            'TOKEN_SECRET',
+                            { expiresIn: '24h' }
+                        )
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+ };
